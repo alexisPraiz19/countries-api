6b51d39b85304dab6e40a3e0081422968a529933
Alexis Praiz <alexispraiz18@gmail.com>
2024-08-30 20:04:19 -0300

    dist

--- src/router_components/forApp/Loading.tsx
+++ src/router_components/forApp/Loading.tsx
@@ -1,7 +1,7 @@
 export default function Loading():JSX.Element{
     return(
         <figure className="loading d-grid">
-            <img src="/assets/svg/loading.svg" className="icon-loading"/>
+            <img src="assets/svg/loading.svg" className="icon-loading"/>
             <figcaption>Loading...</figcaption>
         </figure>
     )
